d2bdedd3ac5202eeda987f97d2c43b51cdd1b4df
Aretas <57877668+aretka@users.noreply.github.com>
2020-04-05 21:35:29 +0300

    Add files via upload

---
+++ v05_list/list_header.h
@@ -0,0 +1,42 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+
+#include <bits/stdc++.h>
+#include <vector>
+#include <limits>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+#include <string>
+#include <sys/time.h>
+#include <list>
+
+
+using std::string;
+using std::vector;
+using std::deque;
+using std::list;
+using std::ofstream;
+using std::ifstream;
+using std::sort;
+using std::cout;
+using std::cin;
+using std::endl;
+
+
+struct mokinys{
+string vardas, pavarde;
+vector<int> C;
+int egzaminas;
+float galutinis;
+
+ bool operator<( const mokinys& a) const {
+ return galutinis < a.galutinis;
+ }
+};
+
+string IntToStr(int);
+list<mokinys> skirstykStudentus(list<mokinys>&);
+
+#endif
---
+++ v05_vector/vector_header.h
@@ -0,0 +1,41 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+
+#include <bits/stdc++.h>
+#include <vector>
+#include <limits>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+#include <string>
+#include <sys/time.h>
+
+
+using std::string;
+using std::vector;
+using std::deque;
+using std::list;
+using std::ofstream;
+using std::ifstream;
+using std::sort;
+using std::cout;
+using std::cin;
+using std::endl;
+
+
+struct mokinys{
+string vardas, pavarde;
+vector<int> C;
+int egzaminas;
+float galutinis;
+};
+
+string IntToStr(int );
+vector<mokinys> skirstykStudentus(vector<mokinys>&);
+void GalBalas(deque<mokinys>& );
+bool comparePagalVarda(const mokinys& , const mokinys& );
+bool comparePagalPavarde(const mokinys& , const mokinys& );
+bool comparePagalGalutini(const mokinys& , const mokinys& );
+
+#endif
